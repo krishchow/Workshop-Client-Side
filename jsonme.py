73d644c76f52c60db449f34f723dc3b2774d2850
Krish <krishc8@hotmail.com>
2019-01-13 15:18:10 -0500

    class jsoneditor good

--- jsonme.py
+++ jsonme.py
@@ -1,6 +1,8 @@
 '''
 Source: https://gist.github.com/jul/e9132abe8b5aeea573917191591fb90b
 Credits: https://gist.github.com/jul
+
+I migrated the code to python 3.6 and created a class!
 '''
 import os
 import re
@@ -13,17 +15,6 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 # create a menu & define functions for each menu item
-    
-def open_command():
-    file = filedialog.askopenfile(parent=root,mode='rb',title='Select a file')
-    if file is not None:
-        contents = file.read()
-        clear(text)
-        text.insert('1.0',contents)
-        file.close()
-        clear(res)
-        root.title("Editing %s" % file.name)        
-        log("opened %s" % file.name)
 
 def indent_json():
     if validate_json():
@@ -34,23 +25,6 @@ def indent_json():
         log("Indentation done")
     else: 
         log("please correct errors first")
-        
-def save_command():
-    file = filedialog.asksaveasfile(mode='w')
-    if file is not None:
-        clear(res)
-        try:
-            data = text.get('1.0', tk.END+'-1c')
-            if data or messagebox.askokcancel("Cancel", "Do you really want to save empty content?"):
-                file.write(data)
-                log("saved %s" % file.name)
-                root.title("Editing %s" % file.name)        
-            else:
-                log("save canceled")
-            file.close()
-        except Exception as e:
-            log(e)
-
     
 def clear(text_stuff):
     text_stuff.delete("1.0", tk.END)
@@ -105,27 +79,82 @@ def validate_json():
     log("JSON is valid")
     return True
 
+class JsonEditor(ScrolledText):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bind("<FocusOut>", self.indent_json)
+        self.bind("<Tab>", self.tab)
+        #self.bind("<Key>",self.validate_json)
+        self.delim_re = re.compile(
+        '''
+        line\s(?P<line>\d+)\s
+        column\s(?P<col>\d+)\s
+        \(
+            char\s(?P<before>\d+)
+                (\s\-\s(?P<after>\d+))? # optionally followed by a range
+        \)''', re.VERBOSE)
+
+    def indent_json(self,*args):
+        if self.validate_json():
+            str_json = self.get("1.0",tk.END)
+            clear(self)
+            clear(res)
+            self.insert('1.0', dumps(loads(str_json), indent=4))
+            log("Indentation done")
+        else: 
+            log("please correct errors first")
+
+    def validate_json(self,*args):
+        clear(res)
+        str_json = self.get("1.0",tk.END)
+        self.tag_delete("error")
+        try:
+            loads(str_json)
+        except ValueError as e:
+            clear(res)
+            msg = str(e.args)
+            log(msg)
+            mark = self.delim_re.search(msg)
+            if not mark:
+                mark = dict(before = "0", after = "end", line = "0", col = "0")
+            else:
+                mark = mark.groupdict()
+                mark["after"] =  "1.0 +%sc" % (mark["after"] or (int(mark["before"]) +1))
             
+            before = "1.0 +%(before)sc" % mark
+            has_delim = re.search("Expecting '(?P<delim>.)'", msg)
+            if has_delim:
+                after = mark["after"] = "1.0 +%dc" % (int(mark["before"]) +1)
+            
+                self.insert(before,has_delim.groupdict()["delim"])
+                #text.delete(after)
+            after = "%s" % mark["after"]
+            self.tag_add("error", before, after)
+            self.tag_config("error", background="yellow", foreground="red")
+            return False
+        except Exception as e:
+            log(e)
+            return False
+        log("JSON is valid")
+        return True
+        
+    def tab(self,arg):
+        self.insert(tk.INSERT, " " * 4)
+        return 'break'
 
 pref = dict(padx=5, pady=5)
 root = tk.Tk(className=u"Minimal customized JSON editor")
 upper = tk.Frame(root, relief=tk.GROOVE)
 bottom = tk.Frame(root, relief=tk.GROOVE)
 
-validate = tk.Button(upper, text = "Validate", command=validate_json)
-indent = tk.Button(upper, text = "Indent", command=indent_json)
-text= ScrolledText(upper, width=100, height=40)
-text.bind("<Tab>", tab)
+text= JsonEditor(upper, width=100, height=40)
+#text= ScrolledText(upper, width=100, height=40)
+#text.bind("<Tab>", tab)
 res= ScrolledText(bottom, width=100,height=10)
-menu = tk.Menu(root)
 
+validate = tk.Button(upper, text = "Validate", command=text.validate_json)
+indent = tk.Button(upper, text = "Indent", command=text.indent_json)
 
-root.config(menu=menu)
-filemenu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="Open", command=open_command)
-filemenu.add_command(label="Save", command=save_command)
-filemenu.add_command(label="Exit", command=root.destroy)
 
 res.pack(side=tk.LEFT,**pref)
 upper.pack(side=tk.TOP,**pref)
